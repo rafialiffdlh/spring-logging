65a1d43f07b46c795e6ab364c12b4d4fc7aff14a
rafialiffdlh <rafialiffdlh18@gmail.com>
2024-12-26 21:01:59 +0700

    Initial commit

---
+++ src/main/java/rafialif/spring_logging/SpringLoggingApplication.java
@@ -0,0 +1,13 @@
+package rafialif.spring_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoggingApplication.class, args);
+	}
+
+}
